db5b68aba16b1a219b394c12acfd8a87b49aaa45
Recep Bayraktar <recepbayraktar0@gmail.com>
2022-04-10 18:29:37 +0300

    ok

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -33,11 +33,6 @@ class SettingController extends Controller
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
