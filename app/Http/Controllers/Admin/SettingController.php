94556b0ecb5ecd70b7ef7b07baf201e38c939e9a
Recep Bayraktar <recepbayraktar0@gmail.com>
2022-04-10 21:48:04 +0300

    setting update function changed

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -10,11 +10,6 @@ use Illuminate\Support\Facades\DB;
 
 class SettingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $data = Setting::first();
@@ -29,95 +24,10 @@ class SettingController extends Controller
         return view('admin.setting.setting_edit', [ 'data' => $data]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Setting  $setting
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Setting $setting)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Setting  $setting
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Setting $setting)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Setting  $setting
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Setting $setting)
     {
-        $id = $request->input('id');
-        $data = Setting::first();
-        $columns = Schema::getColumnListing('settings');
-
-       /*  foreach ($columns as $column) {
-            $data->$column = request()->input($column);
-        } */
-        $data->company = request()->input('company');
-        $data->title = request()->input('title');
-        $data->description = request()->input('description');
-        $data->address = request()->input('address');
-        $data->phone = request()->input('phone');
-        $data->fax = request()->input('fax');
-        $data->email = request()->input('email');
-        $data->keywords = request()->input('keywords');
-        $data->smtpserver = request()->input('smtpserver');
-        $data->smtpemail = request()->input('smtpemail');
-        $data->smtppassword = request()->input('smtppassword');
-        $data->smtpport = request()->input('smtpport');
-        $data->facebook = request()->input('facebook');
-        $data->twitter = request()->input('twitter');
-        $data->instagram = request()->input('instagram');
-        $data->youtube = request()->input('youtube');
-        $data->aboutus = request()->input('aboutus');
-        $data->contact = request()->input('contact');
-        $data->references = request()->input('references');
-        $data->status = request()->input('status');
-
-        $data->save();
-        return redirect()->route('adminProduct');
+        $id = Setting::first()['id'];
+       return Controller::foo(new Setting, route("adminSetting"),$id);
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Setting  $setting
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Setting $setting)
-    {
-        //
-    }
-
-
 }
+
